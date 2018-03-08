46463b5384c32fafa99dfa8e50a1c7a684c4752d
Saulius Lukauskas <luksaulius@gmail.com>
2018-03-08 16:02:30 +0100

    added diff that I forgot to commit a while ago

--- publish.sh
+++ publish.sh
@@ -4,13 +4,13 @@ name="lukauskas/bokeh"
 docker push "${name}:latest";
 
 for version in $(cat STABLE_VERSIONS); do
-    tag="-t ${name}:$version";
+    tag="${name}:$version";
     docker push ${tag};
 done
 
 docker push "${name}:dev";
 
 for version in $(cat DEV_VERSIONS); do
-    tag="-t ${name}:$version";
+    tag="${name}:$version";
     docker push ${tag};
 done
