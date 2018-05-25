d58eacd0c61d3ef7b6277f8cb739c54bb1ce4b12
Saulius Lukauskas <luksaulius@gmail.com>
2018-05-25 12:05:05 +0200

    added alpine

--- build.sh
+++ build.sh
@@ -4,12 +4,15 @@ latest=true
 name="lukauskas/bokeh"
 for version in $(cat STABLE_VERSIONS); do
     tag="-t ${name}:$version";
+    alpine_tag="-t ${name}:${version}-alpine"
 
     if [ ${latest} = true ] ; then
         tag="${tag} -t ${name}:latest";
+        alpine_tag="${alpine_tag} -t ${name}:latest-alpine"
     fi
-
     docker build --build-arg VERSION=${version} docker ${tag};
+
+    docker build -f docker/Dockerfile.alpine --build-arg VERSION=${version} docker ${alpine_tag};
 
     latest=false;
 done
